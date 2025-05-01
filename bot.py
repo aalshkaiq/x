9a256977235fa89f565580552764563df5d6e66b
aalshkaiq <aalshka.iq@gmail.com>
2025-05-02 01:56:41 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "أهلًا بك! 👋")
+
+bot.polling(non_stop=True)
